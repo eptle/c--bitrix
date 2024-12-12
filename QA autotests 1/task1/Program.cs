f292f8b54675752022d4a2c0d75893d6ad1aed0b
eptle <btlk04@gmail.com>
2024-12-12 15:17:26 +0200

    qa hw 1

---
+++ QA autotests 1/task1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     Time Conversion
+     Конвертировать AM/PM формат в 24-часовой
+     
+     Input: 07:05:45PM
+     Output: 19:05:45
+     */
+
+    public static string timeConversion(string s)
+    {
+        DateTime time = Convert.ToDateTime(s);
+        return time.ToString("HH:mm:ss");
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        string s = Console.ReadLine();
+
+        string result = Result.timeConversion(s);
+
+        Console.WriteLine(result);
+    }
+}
---
+++ QA autotests 1/task2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Result
+{
+    /*
+    Migratory Birds
+    Найти в списке самый часто встречающийся ID птицы и вывести его. Если таких несколько, вывести минимальный ID
+
+     Input: 1 2 3 4 5 4 3 2 1 3 4
+     Output: 3
+     */
+
+    public static int migratoryBirds(List<int> arr)
+    {
+        Dictionary<int, int> birdCounts = new Dictionary<int, int>();
+        foreach (int bird in arr)
+        {
+            if (birdCounts.ContainsKey(bird))
+            {
+                birdCounts[bird]++;
+            }
+            else
+            {
+                birdCounts[bird] = 1;
+            }
+        }
+
+        int maxCount = 0;
+        int minId = int.MaxValue;
+
+        foreach (var bird in birdCounts)
+        {
+            if (bird.Value > maxCount || (bird.Value == maxCount && bird.Key < minId))
+            {
+                maxCount = bird.Value;
+                minId = bird.Key;
+            }
+        }
+
+        return minId;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        int result = Result.migratoryBirds(arr);
+        Console.WriteLine(result);
+
+    }
+}
